169afc76f4b6f0259788820cb368f4a0be41f939
MungaiKeren <wambukeren@gmail.com>
2019-11-06 13:07:49 +0300

    This commit creates the user model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    auth_token = models.CharField(max_length=256)
+    restart_token = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'users'
