ed7c6cb68f4e1754a420dc836a9bf86d5387f405
kamauvick <kamauvick@outlook.com>
2019-11-07 12:52:52 +0300

    Register the application models with django admin.

--- api/admin.py
+++ api/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Users)
+admin.site.register(Profile)
 admin.site.register(Location)
 admin.site.register(Trip)
-admin.site.register(Trip_passenger)
+admin.site.register(Trip_Passenger)
 admin.site.register(Trip_Requests)
