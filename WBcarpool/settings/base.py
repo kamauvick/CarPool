8311c63463e61ef72dff2cda61519d1d73e7dfcc
kamauvick <kamauvick@outlook.com>
2019-11-11 11:54:37 +0300

    Set allowed hosts to all in the base settings file.

--- WBcarpool/settings/base.py
+++ WBcarpool/settings/base.py
@@ -55,9 +55,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': (
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
