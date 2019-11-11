a9e86ce50facce77835938eac48c59ac3387b805
kamauvick <kamauvick@outlook.com>
2019-11-11 11:58:52 +0300

    Add a url endpoint for the trip_requests.

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import include, path
 
-from .views import LocationsList, UserListView, ProfileListView
+from .views import LocationsList, get_users, ProfileListView, OfferView,RequestView
+
 
 urlpatterns = [
     path('auth/', include('rest_auth.urls')),
     path('auth/register/', include('rest_auth.registration.urls')),
-    path('users/', UserListView.as_view()),
+    path('users/', get_users),
     path('profiles/', ProfileListView.as_view()),
+    path('offers/', OfferView.as_view(), name='createtrip'),
+    path('request/', RequestView.as_view(), name='requesttrip'),
     path('locations/', LocationsList.as_view(), name='locations')
 ]
