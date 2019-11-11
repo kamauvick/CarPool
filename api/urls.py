e70f23421729d37f29c5a741b7178bf7c7cc63a2
kamauvick <kamauvick@outlook.com>
2019-11-11 17:50:37 +0300

    Add router urls and include them on the urlpatterns.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+from .views import LocationsList, get_users, ProfileListView, OfferView,RequestView, TripsView
 
-from .views import LocationsList, get_users, ProfileListView, OfferView,RequestView
 
+router = SimpleRouter()
+router.register('trips',TripsView,base_name='trips-view' )
 
 urlpatterns = [
     path('auth/', include('rest_auth.urls')),
@@ -10,5 +13,7 @@ urlpatterns = [
     path('profiles/', ProfileListView.as_view()),
     path('offers/', OfferView.as_view(), name='createtrip'),
     path('request/', RequestView.as_view(), name='requesttrip'),
-    path('locations/', LocationsList.as_view(), name='locations')
+    path('locations/', LocationsList.as_view(), name='locations'),
+    path("", include(router.urls))
 ]
+
