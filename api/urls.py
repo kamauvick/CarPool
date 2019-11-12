63b68080bedde8dc3af2f0088b8e7b56326521ca
kamauvick <kamauvick@outlook.com>
2019-11-12 14:16:02 +0300

    Add a url for the request viewfunction.

--- api/urls.py
+++ api/urls.py
@@ -1,37 +1,34 @@
 from django.conf.urls import url
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import SimpleRouter
 
-from .views import LocationsList, get_users, ProfileListView, OfferView, RequestView, TripsView
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from .views import get_users, ProfileListView, TripsView, RequestView
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
-
 
 router = SimpleRouter()
 router.register('trips', TripsView, base_name='trips-view')
+router.register('requests', RequestView, base_name='request-view')
 
 urlpatterns = [
     path('auth/', include('rest_auth.urls')),
     path('auth/register/', include('rest_auth.registration.urls')),
     path('users/', get_users),
     path('profiles/', ProfileListView.as_view()),
-    path('offers/', OfferView.as_view(), name='createtrip'),
-    path('request/', RequestView.as_view(), name='requesttrip'),
-    path('locations/', LocationsList.as_view(), name='locations'),
     path("", include(router.urls)),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
