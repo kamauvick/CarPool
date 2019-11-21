738646276201caffda40ba363f66459fcd8a6874
kamauvick <kamauvick@outlook.com>
2019-11-21 09:48:56 +0300

    Update the models.py file and fix the class spacings.

--- api/models.py
+++ api/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
-# Create your models here.
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
+# Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
