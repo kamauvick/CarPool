d9e484a0350e114ce3b55e9143a392902935f433
kamauvick <kamauvick@outlook.com>
2019-11-27 12:02:43 +0300

    Add filters to the trip model on the admin page.

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
+
 from .models import *
 
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(Location)
-admin.site.register(Trip)
 admin.site.register(Trip_Request)
 admin.site.register(Trip_Offers)
+
+
+@admin.register(Trip)
+class Trip(admin.ModelAdmin):
+    exclude = ('',)
+
+    list_display = ("id",
+                    "departure_time", "arrival_time", "destination", "driver", "status", "origin",
+                    "available_seats", "date", 'created_at',)
+
+    list_filter = (
+        'status',
+        'destination',
+        'departure_time',
+    )
+
+    ordering = ("-departure_time",)
+
+    read_only_fields = ("driver", "created_at", "date", "origin",)
