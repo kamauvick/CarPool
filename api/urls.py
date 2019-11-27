a9893e6e24eeb64e26c4ea95ac3f02fe5d8efeac
kamauvick <kamauvick@outlook.com>
2019-11-27 12:05:33 +0300

    Fix spacing on the urls file.

--- api/urls.py
+++ api/urls.py
@@ -25,6 +25,7 @@ schema_view = get_schema_view(
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
     ),
+
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
