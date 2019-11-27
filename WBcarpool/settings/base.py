f617520b5674b50ba1d01b38cd16e72fee2f54fa
kamauvick <kamauvick@outlook.com>
2019-11-27 13:16:51 +0300

    Change the DB settings on the base.py file from DATABASE_URL to DATABASE_URI.

--- WBcarpool/settings/base.py
+++ WBcarpool/settings/base.py
@@ -145,7 +145,7 @@ WSGI_APPLICATION = 'WBcarpool.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': dj_database_url.config(default=config('DATABASE_URI'))
 }
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
